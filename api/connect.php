25f0f8e1e7d7673e49ccc70650fd0da61acca140
Tomas Novak <novak2@komix.cz>
2021-01-07 00:35:45 +0100

    Dibi

--- api/connect.php
+++ api/connect.php
@@ -1,4 +1,39 @@
 <?php
+/*
+require_once '../vendor/autoload.php';
+
+class Connect {
+
+    public function __construct() {
+        $this->connect();
+    }
+
+    private function connect() {
+        try {
+            $data = array(
+                'driver'   => 'mysqli',
+                'host'     => 'localhost',
+                'username' => 'root',
+                'password' => '',
+                'database' => 'angular_db',
+                'charset'  => 'utf8',
+            );
+
+            ini_set('display_errors', '1');
+
+            dibi::connect($data);
+        } catch (\Dibi\Exception $e) {
+            echo get_class($e), ': ', $e->getMessage(), "\n";
+        }
+    }
+
+}
+
+$connect = new Connect();
+*/
+
+
+
 
 // db credentials
 define('DB_HOST', 'localhost');
@@ -21,3 +56,4 @@ function connect() {
 }
 
 $con = connect();
+
--- api/list.php
+++ api/list.php
@@ -1,6 +1,40 @@
 <?php
-
+/*
 require 'connect.php';
+
+class ListCars {
+
+    public function __construct() {
+        $this->getList();
+    }
+
+    private function getList() {
+        $cars = [];
+
+        $select = dibi::select('id, model, price')->from('cars')->fetchAll();
+        $cr = 0;
+
+        if ($select) {
+            foreach ($select as $row) {
+                $cars[$cr]['id'] = $row['id'];
+                $cars[$cr]['model'] = $row['model'];
+                $cars[$cr]['price'] = $row['price'];
+
+                $cr++;
+            }
+
+            echo json_encode(['data'=>$cars]);
+        } else {
+            http_response_code(404);
+        }
+
+    }
+
+}
+
+$listCars = new ListCars();
+*/
+
 
 $cars = [];
 $sql = "SELECT id, model, price FROM cars";
